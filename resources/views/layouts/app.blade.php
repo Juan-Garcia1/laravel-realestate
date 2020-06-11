dfd07397f1047a645d733c6b8d60346f4b5d3ba5
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2020-06-11 12:36:40 -0500

    create layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,68 +13,107 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+    
+    <header class="header">
+        <a class="header__logo" href="#"><img src="" alt="logo"></a>
+        <nav class="nav">
+            <a class="nav__link" href="">Listings</a>
+            <a class="nav__link" href="#">Agents</a>
+            <a class="nav__link" href="#">Contact</a>
+            @guest
+                <a class="nav__link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                @if (Route::has('register'))
+                    <a class="nav__link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                @endif
+            @else
+                <a href="#">{{ Auth::user()->name }}</a>
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+            @endguest
+            <a class="nav__link nav__link--create-listing-btn" href="#"><i class="fa fa-plus nav__link-icon"></i>Create Listing</a>
         </nav>
+        <button class="nav-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    </header>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <main class="main-content">
+        @yield('content')
+    </main>
+
+    <footer class="footer">
+        <div class="footer__col">
+            <h3 class="footer__col-header">About</h3>
+            <p class="footer__col-text">Lorem ipsum  adipisicing elit. Suscipit sit recusandae odit rerum in eos debitis itaque.</p>
+        </div>
+
+        <div class="footer__col">
+            <h3 class="footer__col-header">Quick Links</h3>
+            <ul class="footer__col-list">
+                <li class="footer__col-item"><a class="footer__col-link" href="#">About Us</a></li>
+                <li class="footer__col-item"><a class="footer__col-link" href="#">Terms & Conditions</a></li>
+                <li class="footer__col-item"><a class="footer__col-link" href="#">Support Center</a></li>
+            </ul>
+        </div>
+
+        <div class="footer__col">
+            <h3 class="footer__col-header">Contact Us</h3>
+            <ul class="footer__col-list">
+                <li class="footer__col-item"><a href="#" class="footer__col-link">info@findhouse.com</a></li>
+                <li class="footer__col-item"><a href="#" class="footer__col-link">Collins St West Virginia, 8007, Australlia</a></li>
+                <li class="footer__col-item"><a href="#" class="footer__col-link">246-345-0699</a></li>
+                <li class="footer__col-item"><a href="#" class="footer__col-link">246-345-0695</a></li>
+            </ul>
+        </div>
+
+        <div class="footer__col">
+            <h3 class="footer__col-header">Follow Us</h3>
+            <ul class="footer__col-social-list">
+                <li class="footer__col-social-item"><a href="#" class="footer__col-social-link"><i class="fa fa-facebook footer__col-social-icon"></i></a></li>
+                <li class="footer__col-social-item"><a href="#" class="footer__col-social-link"><i class="fa fa-twitter footer__col-social-icon"></i></a></li>
+                <li class="footer__col-social-item"><a href="#" class="footer__col-social-link"><i class="fa fa-instagram footer__col-social-icon"></i></a></li>
+            </ul>
+
+            <h3 class="footer__col-header">Subscribe</h3>
+            <form class="footer__col-form">
+                <input type="text" placeholder="Your email" class="footer__col-input"/>
+                <button class="footer__col-submit-btn"><i class="fa fa-chevron-right footer__col-submit-icon"></i></button>
+            </form>
+        </div>
+
+    </footer>
+
+    <div class="sub-footer">
+        <ul class="sub-footer__list">
+            <li class="sub-footer__item"><a href="#" class="sub-footer__link">Home</a></li>
+            <li class="sub-footer__item"><a href="#" class="sub-footer__link">Listings</a></li>
+            <li class="sub-footer__item"><a href="#" class="sub-footer__link">Agents</a></li>
+            <li class="sub-footer__item"><a href="#" class="sub-footer__link">Contact</a></li>
+        </ul>
+        <p class="sub-footer__copyright">&copy; 2020 Find House, Inc.</p>
     </div>
+
+    <script>
+        const hamburgerBtn = document.querySelector('.nav-btn');
+        const nav = document.querySelector('.nav');
+        hamburgerBtn.addEventListener('click', function() {            
+            nav.classList.toggle('nav--open')
+        });
+    </script>
 </body>
 </html>
